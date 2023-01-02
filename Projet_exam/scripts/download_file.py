55b313397bff9151508023ddf3dffd23c90ff4ea
Tiana <39351839+TianaDoto@users.noreply.github.com>
2023-01-02 15:04:37 +0100

    Update download_file.py

--- Projet_exam/scripts/download_file.py
+++ Projet_exam/scripts/download_file.py
@@ -1,6 +1,6 @@
 import requests
 
-#download file tour 1
+#download file tour 1 et tour 2
 URL = ["https://www.data.gouv.fr/fr/datasets/r/79b5cac4-4957-486b-bbda-322d80868224", "https://www.data.gouv.fr/fr/datasets/r/4dfd05a9-094e-4043-8a19-43b6b6bbe086"]
 i = 1
 for items in URL:
